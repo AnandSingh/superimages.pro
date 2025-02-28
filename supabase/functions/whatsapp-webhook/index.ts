9a0fce9e1c0f7aef285b4cf74ae5e38b54b3ba91
Anand Singh <anand.krs@gmail.com>
2025-02-28 12:56:08 +0800

    Update index.ts

--- supabase/functions/whatsapp-webhook/index.ts
+++ supabase/functions/whatsapp-webhook/index.ts
@@ -593,7 +593,7 @@ serve(async (req) => {
     )
 
     const genAI = new GoogleGenerativeAI(Deno.env.get('GEMINI_API_KEY') ?? "");
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro-latest" });
 
     if (req.method === 'POST') {
       const body = await req.json()
